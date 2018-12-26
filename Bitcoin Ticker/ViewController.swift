6d4ea146c399b0b5f914d4a80d36fcd7cc0daa5a
Hansa Anuradha <hansaanuradha@Hansas-MacBook-Pro.local>
2018-12-26 17:14:38 +0530

    first commit

---
+++ Bitcoin Ticker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bitcoin Ticker
+//
+//  Created by Hansa Anuradha on 12/26/18.
+//  Copyright © 2018 Hansa Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
